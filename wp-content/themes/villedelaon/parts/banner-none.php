3f5008dfc4a8c85afe818681753757a54d22afb9
Poniav <macadition@gmail.com>
2016-11-15 11:16:53 +0100

    Ajout fond top et modification banner none

--- wp-content/themes/villedelaon/parts/banner-none.php
+++ wp-content/themes/villedelaon/parts/banner-none.php
@@ -9,7 +9,7 @@
 $src = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'banner-featured-image' );
 if($src) { $url = $src[0]; ?>
 <!-- ========== Banner - Featured Image ========== -->
-<div class="jumbotron image-banner banner-featured-image" style="background:url('<?php echo get_site_url(); ?>/wp-content/uploads/2016/10/bg-feature.jpg') no-repeat 0 0 #ffffff;background-size:cover;background-position:center center">
+<div class="jumbotron image-banner banner-featured-image" style="background:url('<?php echo get_site_url(); ?>/wp-content/themes/villedelaon/assets/img/bg-feature.jpg') no-repeat 0 0 #ffffff;background-size:cover;background-position:center center">
     <div class="container">
         <h1 class="block-title wow zoomIn" ><?php echo esc_html(vega_wp_title()); ?><h1>
     </div>
